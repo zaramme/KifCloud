a8e87e3be42f439375a407441c66ab94c9d7a55e
sho gotoh <shogotoh@wufuji-no-MacBook-Air.local>
2015-05-05 21:02:53 +0900

    - adminLTEのプラグイン設定を調整

--- public/AdminLTE/js/AdminLTE/dashboard.js
+++ public/AdminLTE/js/AdminLTE/dashboard.js
@@ -29,25 +29,25 @@ $(function() {
     //bootstrap WYSIHTML5 - text editor
     $(".textarea").wysihtml5();
 
-    $('.daterange').daterangepicker(
-            {
-                ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                    'Last 7 Days': [moment().subtract('days', 6), moment()],
-                    'Last 30 Days': [moment().subtract('days', 29), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-                },
-                startDate: moment().subtract('days', 29),
-                endDate: moment()
-            },
-    function(start, end) {
-        alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    });
+    // $('.daterange').daterangepicker(
+    //         {
+    //             ranges: {
+    //                 'Today': [moment(), moment()],
+    //                 'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+    //                 'Last 7 Days': [moment().subtract('days', 6), moment()],
+    //                 'Last 30 Days': [moment().subtract('days', 29), moment()],
+    //                 'This Month': [moment().startOf('month'), moment().endOf('month')],
+    //                 'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+    //             },
+    //             startDate: moment().subtract('days', 29),
+    //             endDate: moment()
+    //         },
+    // function(start, end) {
+    //     alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    // });
 
     /* jQueryKnob */
-    $(".knob").knob();
+    // $(".knob").knob();
 
     //jvectormap data
     var visitorsData = {
@@ -64,64 +64,64 @@ $(function() {
         "RU": 3000 //Russia
     };
     //World map by jvectormap
-    $('#world-map').vectorMap({
-        map: 'world_mill_en',
-        backgroundColor: "transparent",
-        regionStyle: {
-            initial: {
-                fill: '#e4e4e4',
-                "fill-opacity": 1,
-                stroke: 'none',
-                "stroke-width": 0,
-                "stroke-opacity": 1
-            }
-        },
-        series: {
-            regions: [{
-                    values: visitorsData,
-                    scale: ["#92c1dc", "#ebf4f9"],
-                    normalizeFunction: 'polynomial'
-                }]
-        },
-        onRegionLabelShow: function(e, el, code) {
-            if (typeof visitorsData[code] != "undefined")
-                el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
-        }
-    });
+    // $('#world-map').vectorMap({
+    //     map: 'world_mill_en',
+    //     backgroundColor: "transparent",
+    //     regionStyle: {
+    //         initial: {
+    //             fill: '#e4e4e4',
+    //             "fill-opacity": 1,
+    //             stroke: 'none',
+    //             "stroke-width": 0,
+    //             "stroke-opacity": 1
+    //         }
+    //     },
+    //     series: {
+    //         regions: [{
+    //                 values: visitorsData,
+    //                 scale: ["#92c1dc", "#ebf4f9"],
+    //                 normalizeFunction: 'polynomial'
+    //             }]
+    //     },
+    //     onRegionLabelShow: function(e, el, code) {
+    //         if (typeof visitorsData[code] != "undefined")
+    //             el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+    //     }
+    // });
 
     //Sparkline charts
-    var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
-    $('#sparkline-1').sparkline(myvalues, {
-        type: 'line',
-        lineColor: '#92c1dc',
-        fillColor: "#ebf4f9",
-        height: '50',
-        width: '80'
-    });
-    myvalues = [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921];
-    $('#sparkline-2').sparkline(myvalues, {
-        type: 'line',
-        lineColor: '#92c1dc',
-        fillColor: "#ebf4f9",
-        height: '50',
-        width: '80'
-    });
-    myvalues = [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21];
-    $('#sparkline-3').sparkline(myvalues, {
-        type: 'line',
-        lineColor: '#92c1dc',
-        fillColor: "#ebf4f9",
-        height: '50',
-        width: '80'
-    });    
+    // var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
+    // $('#sparkline-1').sparkline(myvalues, {
+    //     type: 'line',
+    //     lineColor: '#92c1dc',
+    //     fillColor: "#ebf4f9",
+    //     height: '50',
+    //     width: '80'
+    // });
+    // myvalues = [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921];
+    // $('#sparkline-2').sparkline(myvalues, {
+    //     type: 'line',
+    //     lineColor: '#92c1dc',
+    //     fillColor: "#ebf4f9",
+    //     height: '50',
+    //     width: '80'
+    // });
+    // myvalues = [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21];
+    // $('#sparkline-3').sparkline(myvalues, {
+    //     type: 'line',
+    //     lineColor: '#92c1dc',
+    //     fillColor: "#ebf4f9",
+    //     height: '50',
+    //     width: '80'
+    // });    
 
     //The Calender
-    $("#calendar").datepicker();
+    // $("#calendar").datepicker();
 
     //SLIMSCROLL FOR CHAT WIDGET
-    $('#chat-box').slimScroll({
-        height: '250px'
-    });
+    // $('#chat-box').slimScroll({
+    //     height: '250px'
+    // });
 
     /* Morris.js Charts */
     // Sales chart
