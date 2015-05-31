7213a5ec0d7d51203c6c022792d76e30a86b5cd2
sho gotoh <shogotoh@wufuji-no-MacBook-Air-2.local>
2015-05-31 21:24:49 +0900

    - 局面リンク、棋譜読み込みビューのレイアウト変更

--- public/js/panelInfo.js
+++ public/js/panelInfo.js
@@ -1,10 +1,15 @@
 $(function(){
 debug("panelInfo.jsを読み込みました");
 
+const RSH_BOARD = "localhost:9000/board/"
 
 $("#RshPanel").on("reloadPanel", function(ev){
 	debug("rshPanelをリロードシミアス。");
-	$("#RshPanel").children("input").attr("value", document.info.RshCurrent.value)
+	if (document.info.RshCurrent == undefined)
+		return
+
+	url = RSH_BOARD + document.info.RshCurrent.value
+	$("#RshPanel").children("textarea").html(url);
 })
 });
 
