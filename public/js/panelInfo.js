e3566316c26622c108bfac66d953f1097a1ba447
sho gotoh <shogotoh@wufuji-no-MacBook-Air-2.local>
2015-06-17 09:17:28 +0900

    - twitterシェア機能を追加

--- public/js/panelInfo.js
+++ public/js/panelInfo.js
@@ -1,14 +1,23 @@
 $(function(){
-debug("panelInfo.jsを読み込みました");
 
+	//クリック時に全選択
+	$("#RshPanel").children("textarea").click(function(){
+	    $(this).select();
+  	});
 
 $("#RshPanel").on("reloadPanel", function(ev){
-	debug("rshPanelをリロードシミアス。");
 	if (document.info.RshCurrent == undefined)
 		return
 
-	url = getHost() + 'board/' + document.info.RshCurrent.value
+	// テキストエリアのリロード
+	//var url = getHost() + 'board/' + document.info.RshCurrent.value
+	var url = 'http://kifcloud.info/board/' + document.info.RshCurrent.value
 	$("#RshPanel").children("textarea").html(url);
-})
-});
 
+	// twitterシェアボタンのリロード
+	var tweetinput = '<a href="https://twitter.com/share" class="twitter-share-button"  data-url="'+url+'" data-text="【#将棋局面シェア】" data-lang="ja" data-count="none">局面をツイート</a>';
+	$("#RshPanel").children(".tweetButtonWrapper").html(tweetinput);
+	twttr.widgets.load();
+	});
+
+});
